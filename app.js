16a3c64c55b2174eab4b882b08d2dca4e0ab4b2d
Sean Bartholomew <bart.sean13@gmail.com>
2017-05-24 13:22:51 -0600

    server adding back in openshift health response

--- app.js
+++ app.js
@@ -6,6 +6,44 @@ const http         = require('http'),
 	  sysInfo      = require('./utils/sys-info'),
 	  env          = process.env;
 
+let server = http.createServer(function (req, res) {
+  let url = req.url;
+  if (url == '/') {
+    url += 'index.html';
+  }
+
+  // IMPORTANT: Your application HAS to respond to GET /health with status 200
+  //            for OpenShift health monitoring
+
+  if (url == '/health') {
+    res.writeHead(200);
+    res.end();
+  } else if (url == '/info/gen' || url == '/info/poll') {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 'no-cache, no-store');
+    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+  } else {
+    fs.readFile('./static' + url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end('Not found');
+      } else {
+        let ext = path.extname(url).slice(1);
+        if (contentTypes[ext]) {
+          res.setHeader('Content-Type', contentTypes[ext]);
+        }
+        if (ext === 'html') {
+          res.setHeader('Cache-Control', 'no-cache, no-store');}
+          res.end(data);
+      }
+    });
+  }
+});
+
+server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+  console.log(`Application worker ${process.pid} started...`);
+});
+
 
 /**
  * WebSocket server
@@ -17,7 +55,7 @@ wss.on('connection', function connection(ws) {
     console.log('received: %s', message);
   });
 
-  ws.send(JSON.stringify({"oh": "hello there"}));
+  ws.send(JSON.stringify({"oh": "hello th"}));
 });
 
 wss.broadcast = function broadcast(data) {
